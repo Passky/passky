98b06dc917e674dca53a55714e26f16711e60d20
root <root@Passky.localdomain>
2020-02-09 11:52:55 +0800

    sync

--- PY/he.py
+++ PY/he.py
@@ -1,13 +1,2 @@
-def triangles(x):
-    b=[1]
-    if x == 1:
-        yield 1
-    if x == 2:
-        for i in range(2):
-            yield 1
-    else:
-        one.clear()
-        one.append = triangles(x-1)
-        yield 1
-        for i in one[0:-1]:
-            yield 
+class student(object):
+    def __init
--- PY/sample.py
+++ PY/sample.py
@@ -1,11 +1,32 @@
-import random
-import functools
-l1 = []
-for i in range(5):
-    l1.append(random.uniform(1.0, 10.0))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+class Student(object):
 
-def cmp(x): return -x
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
 
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
 
-print(sorted(l1, key=cmp))
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+class shit(Student):
+    
+fuck = shit('wu',100)
+fuck.print_score()
+#print('bart.name =', bart.name)
+#print('bart.score =', bart.score)
+#bart.print_score()
+
+#print('grade of Bart:', bart.get_grade())
+#print('grade of Lisa:', lisa.get_grade())
