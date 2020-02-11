1ec93ad8f51332734cfe7b1c30ca5173e484cd75
root <root@Passky.localdomain>
2020-02-11 13:23:43 +0800

    sync

--- PY/sample.py
+++ PY/sample.py
@@ -1,19 +1,7 @@
-import functools
-class shity(object):
-    @property
-    def score(self):
-        return self._score
-    @score.setter
-    def score(self,value):
-        if not isinstance(value,int):
-            raise ValueError('Score should be an integer')
-        elif value<0 or value>100:
-            raise ValueError('Should be between 0~100')
-        self._score=value
-class shake(object):
-    def __getitem__(self,n):
-        print('Life die')
-class spicy(shity,shake):
-    pass
-s=spicy()
-s[4]
+import enum
+@enum.unique
+class shit(enum.Enum):
+    avegege=1
+    fuck=2
+s=shit
+print(s.avegege)
