ce6679c1b9f023fb612be66630ff3fc3e06900bf
passky <cmpassky@outlook.com>
2020-02-16 21:03:16 +0800

    sync

--- c/list.h
+++ c/list.h
@@ -8,10 +8,11 @@
     {              \
         x = x + y; \
         y = x - y; \
-        x = x - y;}
+        x = x - y; \
+    }
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 struct data2
 {
     int no;
@@ -25,19 +26,37 @@ struct node
 };
 typedef struct node link;
 void lstart(link *list);          //done
-int additem(data da, link *list); //done
+int additem(link *list, int v) //done
+{
+   link* x=(link*)malloc(sizeof(link));
+   x->sample=v;
+   x->next=NULL;
+   list->next=x;
+   return v;
+}
 int empty(link *list);            //meanless
 int counting(link *list);         //done
 int full(link *list);             //nope
 link converse(link *list);        //done
-void delete(link* list,link *shit)
+link* deletenode (link *list, link *shit)
 {
     link *start = list;
     link *follow = start;
     start = start->next;
-    while (start->next!=NULL)
+    while (start != shit && start != NULL)
     {
-        if()
+        start = start->next;
+        follow = follow->next;
+    }
+    if (start == NULL)
+        printf("Error!\n");
+    else if (start->next == NULL)
+
+        follow->next = NULL;
+    else
+    {
+        follow->next = start->next;
+        free(start);
     }
 }
 //link delete (link list);
--- c/nwe.c
+++ c/nwe.c
@@ -1,26 +1,10 @@
 #include "list.h"
+#include"MYCMP.h"
+#define anv sample
+MYMAL((link*))
 int main()
 {
-    link start;
-    link *origin = &start;
-    for (int i = 0; i < 5; i++)
-    {
-        origin->next = (link *)malloc(sizeof(link));
-        origin->next->sample = i + 1;
-        origin = origin->next;
-    }
-    origin->next = NULL;
-    int i = 0;
-    for (link *re = start.next; re != NULL; re->next)
-    {
-        i++;
-        if (i == 2)
-        {
-            re = NULL;
-            break;
-        }
-    }
-
-    for (link *re = &start; re->next != 0; re = re->next)
-        printf("%d\n", re->next->sample);
+   link*** a = _malloc2d(3, 2);
+   a[1][2]->sample =3 ;
+   printf("%d\n", a[1][2]->sample);
 }
